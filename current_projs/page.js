db80905d70e12ade4e27435268b7064e39d0286e
RosenlindDylan <35314066+RosenlindDylan@users.noreply.github.com>
2023-08-07 21:55:22 -0700

    fix next.js deployment

--- current_projs/page.js
+++ current_projs/page.js
@@ -1,10 +1,15 @@
-import Head from 'next/head';
+'use client'
+
+import React, { useEffect } from "react";
+import Suggestion from "./suggestion";
 
 export default function Home() {
+
   return (
     <>
       <body>
         <h1>Welcome to hangout!</h1>
+        <Suggestion />
       </body>
     </>
   )
